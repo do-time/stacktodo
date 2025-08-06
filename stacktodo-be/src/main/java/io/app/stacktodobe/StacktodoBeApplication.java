e5d584acbfd22738f5bc997fa089f1cb5f561f1d
LEEHYUNDONG <l.steve10@yahoo.com>
2025-08-07 08:49:41 +0900

    initial commit

---
+++ stacktodo-be/src/main/java/io/app/stacktodobe/StacktodoBeApplication.java
@@ -0,0 +1,13 @@
+package io.app.stacktodobe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StacktodoBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StacktodoBeApplication.class, args);
+    }
+
+}
---
+++ stacktodo-be/src/test/java/io/app/stacktodobe/StacktodoBeApplicationTests.java
@@ -0,0 +1,13 @@
+package io.app.stacktodobe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StacktodoBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
