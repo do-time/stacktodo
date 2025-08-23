4844f5548ac631890d813defd6c48797a41a577e
hoonbro <wlgns1588@naver.com>
2025-08-23 18:34:49 +0900

    feat: [20250823] 오타 수정

--- stacktodo-be/src/main/java/io/app/stacktodobe/tasklist/persistence/repository/TaskListRepository.java
+++ stacktodo-be/src/main/java/io/app/stacktodobe/tasklist/persistence/repository/TaskListRepository.java
@@ -3,5 +3,5 @@ package io.app.stacktodobe.tasklist.persistence.repository;
 import io.app.stacktodobe.tasklist.persistence.entity.TaskList;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TaskListRepository extends JpaRepository<Long, TaskList> {
+public interface TaskListRepository extends JpaRepository<TaskList, Long> {
 }
